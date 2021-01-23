48daf3a2ecfa67cdc1091e3e7a20d3c1ca2e2828
ANeshkova <anitowurth@abv.bg>
2021-01-23 09:51:36 +0000

    Added solution

---
+++ 01.1 Working with Abstraction - Lab/02. PointInRectangle/pointInRectangle/Main.java
@@ -0,0 +1,29 @@
+package pointInRectangle;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] coordinates = Arrays.stream(scan.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        Point bottomLeft = new Point(coordinates[0], coordinates[1]);
+        Point topRight = new Point(coordinates[2], coordinates[3]);
+
+        Rectangle rectangle = new Rectangle(bottomLeft, topRight);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        while (n-- > 0) {
+            coordinates = Arrays.stream(scan.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt).toArray();
+
+            Point point = new Point(coordinates[0], coordinates[1]);
+
+            System.out.println(rectangle.contains(point));
+        }
+    }
+}
