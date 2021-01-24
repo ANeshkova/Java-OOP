1e738412ece377ca77db18ff33e718acc45b2f44
ANeshkova <anitowurth@abv.bg>
2021-01-24 10:07:12 +0000

    Added solution

---
+++ 01.1 Working with Abstraction - Lab/04. HotelReservation/hotelReservation/Main.java
@@ -0,0 +1,18 @@
+package hotelReservation;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] input = scan.nextLine().split("\\s+");
+
+        double pricePerDay = Double.parseDouble(input[0]);
+        int numberOfDays = Integer.parseInt(input[1]);
+        Season season = Season.valueOf(input[2]);
+        Discount discountType = Discount.valueOf(input[3]);
+
+        System.out.println(PriceCalculator.CalculatePrice(pricePerDay, numberOfDays, season, discountType));
+    }
+}
