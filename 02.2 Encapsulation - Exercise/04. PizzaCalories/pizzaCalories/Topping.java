5395722aafefb617c9caec4ae44f26ae11cb8f66
ANeshkova <anitowurth@abv.bg>
2021-02-05 11:37:41 +0000

    Added solution

---
+++ 02.2 Encapsulation - Exercise/04. PizzaCalories/pizzaCalories/Topping.java
@@ -0,0 +1,30 @@
+package pizzaCalories;
+
+public class Topping {
+    private String toppingType;
+    private double weight;
+
+    public Topping(String toppingType, double weight) {
+        this.setToppingType(toppingType);
+        this.setWeight(weight);
+    }
+
+    private void setToppingType(String toppingType) {
+        if (TypesAndTechniques.TOPPING_TYPES.containsKey(toppingType)) {
+            this.toppingType = toppingType;
+        } else {
+            throw new IllegalArgumentException("Cannot place " + toppingType + " on top of your pizza.");
+        }
+    }
+
+    private void setWeight(double weight) {
+        if (weight < 1 || weight > 50) {
+            throw new IllegalArgumentException(this.toppingType + " weight should be in the range [1..50].");
+        }
+        this.weight = weight;
+    }
+
+    public double calculateCalories() {
+        return this.weight * 2 * TypesAndTechniques.TOPPING_TYPES.get(this.toppingType);
+    }
+}
