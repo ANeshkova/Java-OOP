c98c8594fdb996f97c2179737233d6404f73cc84
ANeshkova <anitowurth@abv.bg>
2021-02-06 10:56:00 +0000

    Added solution

---
+++ 02.2 Encapsulation - Exercise/05. FootballTeamGenerator/footballTeamGenerator/Main.java
@@ -0,0 +1,52 @@
+package footballTeamGenerator;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Map<String, Team> teams = new HashMap<>();
+
+        String input = scan.nextLine();
+
+        while (!"END".equals(input)) {
+            String[] tokens = input.split(";");
+            String command = tokens[0];
+            String teamName = tokens[1];
+
+            try {
+                if (command.equals("Team")) {
+                    teams.putIfAbsent(teamName, new Team(teamName));
+
+                } else if (command.equals("Add")) {
+                    if (!teams.containsKey(teamName)) {
+                        throw new IllegalArgumentException("Team " + teamName + " does not exist.");
+                    }
+
+                    teams.get(teamName).addPlayer(new Player(tokens[2],
+                            Integer.parseInt(tokens[3]),
+                            Integer.parseInt(tokens[4]),
+                            Integer.parseInt(tokens[5]),
+                            Integer.parseInt(tokens[6]),
+                            Integer.parseInt(tokens[7])));
+
+                } else if (command.equals("Remove")) {
+                    teams.get(teamName).removePlayer(tokens[2]);
+
+                } else {
+                    if (!teams.containsKey(teamName)) {
+                        throw new IllegalArgumentException("Team " + teamName + " does not exist.");
+                    }
+
+                    System.out.println(teamName + " - " + (int) teams.get(teamName).getRating());
+                }
+
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+            input = scan.nextLine();
+        }
+    }
+}
