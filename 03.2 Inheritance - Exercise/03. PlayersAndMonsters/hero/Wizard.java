bca562c1492a90f08cacc0a4b36b33b1f2781913
ANeshkova <anitowurth@abv.bg>
2021-02-16 12:58:16 +0000

    Added solution

---
+++ 03.2 Inheritance - Exercise/03. PlayersAndMonsters/hero/Wizard.java
@@ -0,0 +1,13 @@
+package hero;
+
+public class Wizard extends Hero {
+
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
