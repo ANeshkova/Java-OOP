201e2aa3946dfe77597b5274344cd7b61a62a98a
ANeshkova <anitowurth@abv.bg>
2021-03-02 16:19:01 +0000

    Added solution

---
+++ 04.1 Interfaces and Abstraction - Lab/06. Ferrari/ferrari/Main.java
@@ -0,0 +1,15 @@
+package ferrari;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.nextLine();
+
+        Car car = new Ferrari(name);
+
+        System.out.println(car.toString());
+    }
+}
