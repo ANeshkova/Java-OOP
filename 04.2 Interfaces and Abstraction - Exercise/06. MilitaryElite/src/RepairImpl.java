35e129869912aed4503c5a7394ed02eb6762d4b0
ANeshkova <anitowurth@abv.bg>
2021-03-11 13:44:58 +0000

    Added solution

---
+++ 04.2 Interfaces and Abstraction - Exercise/06. MilitaryElite/src/RepairImpl.java
@@ -0,0 +1,24 @@
+public class RepairImpl implements Repair {
+    private String partName;
+    private int hoursWorked;
+
+    public RepairImpl(String partName, int hoursWorked) {
+        this.partName = partName;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public String getName() {
+        return this.partName;
+    }
+
+    @Override
+    public int getHoursWorked() {
+        return this.hoursWorked;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("  Part Name: %s Hours Worked: %d", getName(), getHoursWorked());
+    }
+}
