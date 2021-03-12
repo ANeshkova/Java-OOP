972504a8060bc64234cac746a3cf05608abafad8
ANeshkova <anitowurth@abv.bg>
2021-03-12 12:51:12 +0000

    Added solution

---
+++ 04.2 Interfaces and Abstraction - Exercise/06. MilitaryElite/src/SpecialisedSoldierImpl.java
@@ -0,0 +1,20 @@
+public abstract class SpecialisedSoldierImpl extends PrivateImpl implements SpecialisedSoldier {
+    private Corps corps;
+
+    public SpecialisedSoldierImpl(int id, String firstName, String lastName, double salary, Corps corps) {
+        super(id, firstName, lastName, salary);
+        this.corps = corps;
+    }
+
+    @Override
+    public Corps getCorps() {
+        return this.corps;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.lineSeparator()).append("Corps: " + getCorps().toString());
+        return super.toString() + sb.toString();
+    }
+}
