1a2315fede3003d336718637064b902963c26bfe
ANeshkova <anitowurth@abv.bg>
2021-03-14 13:03:26 +0000

    Added solution

---
+++ 04.2 Interfaces and Abstraction - Exercise/07. CollectionHierarchy/collectionHierarchy/AddRemoveCollection.java
@@ -0,0 +1,14 @@
+package collectionHierarchy;
+
+public class AddRemoveCollection extends Collection implements AddRemovable {
+
+    @Override
+    public String remove() {
+        return super.remove(true);
+    }
+
+    @Override
+    public int add(String str) {
+        return super.add(str, false);
+    }
+}
