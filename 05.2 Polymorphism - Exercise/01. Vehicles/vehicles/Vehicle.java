b635742efd743a1371c9bef57831c25b153f8d6d
ANeshkova <anitowurth@abv.bg>
2021-03-18 14:53:47 +0000

    Added solution

---
+++ 05.2 Polymorphism - Exercise/01. Vehicles/vehicles/Vehicle.java
@@ -0,0 +1,35 @@
+package vehicles;
+
+import java.text.DecimalFormat;
+
+public abstract class Vehicle {
+    private double fuelQuantity;
+    private double fuelConsumption;
+
+    Vehicle(double fuelQuantity, double fuelConsumption) {
+        this.fuelQuantity = fuelQuantity;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void drive(double km) {
+        DecimalFormat format = new DecimalFormat("#.##");
+
+        if (this.fuelConsumption * km <= this.fuelQuantity) {
+
+            this.fuelQuantity = this.fuelQuantity - (this.fuelConsumption * km);
+            System.out.println(String.format("%s travelled %s km", this.getClass().getSimpleName(), format.format(km)));
+
+        } else {
+            System.out.println(String.format("%s needs refueling", this.getClass().getSimpleName()));
+        }
+    }
+
+    public void refuel(double fuel) {
+        this.fuelQuantity += fuel;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %.2f", this.getClass().getSimpleName(), this.fuelQuantity);
+    }
+}
