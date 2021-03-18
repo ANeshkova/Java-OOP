9c345273a84437bc31dc9351040ef1c78b33673e
ANeshkova <anitowurth@abv.bg>
2021-03-18 14:53:16 +0000

    Added solution

---
+++ 05.2 Polymorphism - Exercise/01. Vehicles/vehicles/Car.java
@@ -0,0 +1,11 @@
+package vehicles;
+
+public class Car extends Vehicle {
+    private double fuelQuantity;
+    private double fuelConsumption;
+    private final static double SUMMER_CONSUMPTION = 0.9;
+
+    public Car(double fuelQuantity, double fuelConsumption) {
+        super(fuelQuantity, fuelConsumption + SUMMER_CONSUMPTION);
+    }
+}
